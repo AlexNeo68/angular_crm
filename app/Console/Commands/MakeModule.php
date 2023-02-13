47311e1b7b274b5950046e1e5c6c4e2fbebf77b8
alexneo68 <tunning_brain@mail.ru>
2023-02-13 21:37:49 +0300

    make module

---
+++ app/Console/Commands/MakeModule.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class MakeModule extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:module {--all} {--migration} {--view} {--api} {--vue} {--controller} {--model}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        return Command::SUCCESS;
+    }
+}
