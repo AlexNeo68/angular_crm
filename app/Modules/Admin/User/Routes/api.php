fa995641061545584265eb3975c9c6e28479d103
alexneo68 <tunning_brain@mail.ru>
2023-02-14 20:33:56 +0300

    modular provider

---
+++ app/Modules/Admin/User/Routes/api.php
@@ -0,0 +1,10 @@
+<?php
+use Illuminate\Support\Facades\Route;
+
+Route::group(['prefix' => 'users', 'middleware' => []], function () {
+    Route::get('/', 'Api\UserController@index')->name('api.users.index');
+    Route::post('/', 'Api\UserController@store')->name('api.users.store');
+    Route::get('/{user}', 'Api\UserController@show')->name('api.users.read');
+    Route::put('/{user}', 'Api\UserController@update')->name('api.users.update');
+    Route::delete('/{user}', 'Api\UserController@destroy')->name('api.users.delete');
+});
