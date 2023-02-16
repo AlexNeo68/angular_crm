ac7239d5e8222eaf08d040e550725a5e02cec734
Александр Неудахин <aleksandrneudahin@Aleksandrs-Air.localnet>
2023-02-16 17:57:46 +0300

    api auth

--- app/Modules/Pub/Auth/Controllers/Api/LoginController.php
+++ app/Modules/Pub/Auth/Controllers/Api/LoginController.php
@@ -3,12 +3,32 @@
 namespace App\Modules\Pub\Auth\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Modules\Admin\User\Requests\LoginRequest;
+use App\Services\Responses\ResponseService;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    public function login(Request $request)
+    public function login(LoginRequest $request)
     {
-        return $request;
+        $credentials = request(['email', 'password']);
+
+        if(!Auth::attempt($credentials)){
+            return ResponseService::sendJsonResponse(false, 403, [
+                'message' => __('auth.failed')
+            ] );
+        }
+
+        $user = Auth::user();
+
+        $tokenResult = $user->createToken('Personal Access Token');
+        return ResponseService::sendJsonResponse(true, 200, [], [
+            'api_token' => $tokenResult->accessToken,
+            'user' => $user,
+            'token_type' => 'Bearer',
+            'expires_at' => Carbon::parse($tokenResult->token->expires_at)->toDateTimeString()
+        ]);
     }
 }
