d200f8c7a57e99b866487d0e4dea0390b9e45e1a
alexneo68 <tunning_brain@mail.ru>
2023-02-22 16:13:02 +0300

    api users

--- app/Modules/Admin/User/Controllers/Api/UserController.php
+++ app/Modules/Admin/User/Controllers/Api/UserController.php
@@ -5,6 +5,7 @@ namespace App\Modules\Admin\User\Controllers\Api;
 use App\Modules\Admin\User\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Modules\Admin\User\Requests\UserRequest;
 use App\Modules\Admin\User\Services\UserService;
 use App\Services\Responses\ResponseService;
 
@@ -29,69 +30,64 @@ class UserController extends Controller
         ]);
     }
 
-    /**
-     * Create of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+
+    public function store(UserRequest $request)
     {
-        //
+
+        $user = $this->service->save($request, new User());
+
+        return ResponseService::sendJsonResponse(true, 200, [], [
+            'user' =>  $user->toArray()
+        ]);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Modules\Admin\User\Models\User  $user
-     * @return \Illuminate\Http\Response
-     */
+
     public function show(User $user)
     {
-        //
+        return ResponseService::sendJsonResponse(true, 200,[],[
+            'user' =>  $user->toArray()
+        ]);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Modules\Admin\User\Models\User  $user
-     * @return \Illuminate\Http\Response
-     */
+
     public function edit(User $user)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Modules\Admin\User\Models\User  $user
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, User $user)
+
+    public function update(UserRequest $request, User $user)
     {
-        //
+        $user = $this->service->save($request, $user);
+        return ResponseService::sendJsonResponse(true, 200,[],[
+            'user' =>  $user->toArray()
+        ]);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Modules\Admin\User\Models\User  $user
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(User $user)
     {
+        $user->status = '0';
+        $user->update();
+
+        return ResponseService::sendJsonResponse(true, 200,[],[
+            'user' =>  $user->toArray()
+        ]);
+    }
+
+    public function usersForm() {
         //
+        $this->authorize('view', new User());
+
+        $users = $this->service->getUsers(1);
+
+        return ResponseService::sendJsonResponse(true, 200,[],[
+            'users' =>  $users->toArray()
+        ]);
     }
 }
